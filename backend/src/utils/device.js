f22da1884200f5d4635a4fcc609c4fbf7bbb9266
annamalaiyarcodeverse <annamalaiyarcodeverse@gmail.com>
2026-02-06 21:30:58 +0530

    initial split frontend and backend

--- backend/src/utils/device.js
+++
@@ -1,8 +0,0 @@
-export function getDeviceId() {
-  let id = localStorage.getItem("device_id");
-  if (!id) {
-    id = crypto.randomUUID();
-    localStorage.setItem("device_id", id);
-  }
-  return id;
-}
