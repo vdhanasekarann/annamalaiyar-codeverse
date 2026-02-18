c0ef436c2b9daebcac872873a81bd61f5dbe8716
annamalaiyarcodeverse <vdhanasekarann@gmail.com>
2026-02-18 23:57:04 +0530

    Update backend 18

--- src/components/GPTCard.jsx
+++ src/components/GPTCard.jsx
@@ -5,6 +5,7 @@ import { Navigate, useNavigate, Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import clsx from 'clsx';
 import GlassCard from './GlassCard';
+import CountUp from './ui/CountUp';
 
 function GPTCard({ gpt, used, plan, onUsed, theme = 'pink' }) {
   const { t } = useTranslation();
